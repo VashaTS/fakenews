c2c0677d7f8f71f25fd16723da720b8721d8c3c7
sajmon313@gmail.com <darthv4der>
2020-03-21 12:08:46 +0100

    Updates

---
+++ precache-manifest.71e89abf13bd44236d166b86313bad56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9654f3cb5150d60a620d53df69ea86c2",
+    "url": "/fakenews/index.html"
+  },
+  {
+    "revision": "25150f73e88552886668",
+    "url": "/fakenews/static/css/main.3a2bcc5f.chunk.css"
+  },
+  {
+    "revision": "d5ee291cd620e4701758",
+    "url": "/fakenews/static/js/2.d5b05a84.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/fakenews/static/js/2.d5b05a84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25150f73e88552886668",
+    "url": "/fakenews/static/js/main.7ebf0a06.chunk.js"
+  },
+  {
+    "revision": "7f9190f703e215b37fba",
+    "url": "/fakenews/static/js/runtime-main.3f53b625.js"
+  }
+]);
